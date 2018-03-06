bd014d02cb4d45fde026a74503685f62db6d4073
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-06 17:33:01 +0300

    welcome message is now coming from application.properties

--- src/main/java/com/trendyol/controller/WelcomeController.java
+++ src/main/java/com/trendyol/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package com.trendyol.controller;
 
 import com.trendyol.service.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,12 @@ public class WelcomeController {
     @Autowired
     private WelcomeService welcomeService;
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @RequestMapping("/welcome")
     public String welcome(){
-        return welcomeService.retrieveWelcomeMessage();
+        return welcomeMessage;
+        //return welcomeService.retrieveWelcomeMessage();
     }
 }
