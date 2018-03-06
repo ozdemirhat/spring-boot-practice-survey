28d8ad3bbab491babea43a78e05bbf73859fdc66
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-06 09:48:25 +0300

    component scan is commented

--- src/main/java/com/trendyol/Application.java
+++ src/main/java/com/trendyol/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.trendyol"} )
+//@ComponentScan(basePackages = { "com.trendyol"} )
 public class Application {
 
     public static void main(String[] args) {
