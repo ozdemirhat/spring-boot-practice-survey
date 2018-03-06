e9c92f9205dbe16954245470a0dfce8737ae312d
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-06 11:42:46 +0300

    add question to survey is added

--- src/main/java/com/trendyol/controller/SurveyController.java
+++ src/main/java/com/trendyol/controller/SurveyController.java
@@ -4,11 +4,11 @@ package com.trendyol.controller;
 import com.trendyol.model.Question;
 import com.trendyol.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,6 +25,19 @@ public class SurveyController {
     @RequestMapping(value="/surveys/{surveyId}/questions/{questionId}", method= RequestMethod.GET)
     public Question retrieveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId){
         return surveyService.retrieveQuestion(surveyId, questionId);
+    }
+
+    @RequestMapping(value="/surveys/{surveyId}/questions", method= RequestMethod.POST)
+    public ResponseEntity addQuestionToSurvey (@PathVariable String surveyId, @RequestBody Question newQuestion){
+        Question question =surveyService.addQuestion(surveyId, newQuestion);
+
+        if(question == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(question.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
