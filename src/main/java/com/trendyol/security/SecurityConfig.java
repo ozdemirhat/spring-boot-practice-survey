76678750e8c24daaff57aefb81c89f882f9e5a5e
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-07 16:29:08 +0300

    code is organized

--- src/main/java/com/trendyol/security/SecurityConfig.java
+++ src/main/java/com/trendyol/security/SecurityConfig.java
@@ -12,16 +12,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication().withUser("user1").password("secret1")
-                .roles("USER").and().withUser("admin1").password("secret1")
-                .roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("user1").password("{noop}secret1").roles("USER")
+                .and()
+                .withUser("admin1").password("{noop}secret1").roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests().antMatchers("/surveys/**")
-                .hasRole("USER").antMatchers("/users/**").hasRole("USER")
-                .antMatchers("/**").hasRole("ADMIN").and().csrf().disable()
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers("/surveys/**").hasRole("USER")
+                .antMatchers("/users/**").hasRole("USER")
+                .antMatchers("/**").hasRole("ADMIN")
+                .and().csrf().disable()
                 .headers().frameOptions().disable();
     }
 }
